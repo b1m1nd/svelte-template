f471f2e5a9d51e6e832574666c656b154c7b27e2
b1mind <brent@1mindsolutions.com>
2020-05-31 04:07:30 -0600

    👍Added FontAwesome5 📄fa-svelte

--- src/routes/projects.svelte
+++ src/routes/projects.svelte
@@ -1,11 +1,21 @@
 <script>
-
+  import Icon from 'fa-svelte'
+  import { faArrowsAlt, faArrowDown, faFeather, faBacon, faFish } from '@fortawesome/free-solid-svg-icons'
 </script>
 
 <svelte:head>
   <title>Projects</title>
 </svelte:head>
 <p>just some projects</p>
+<p>
+  <Icon icon={faArrowDown} />
+  you can use fontAwesome too !
+</p>
+
+<Icon icon={faArrowsAlt} />
+<Icon icon={faFeather} />
+<Icon icon={faFish} />
+<Icon icon={faBacon} />
 
 <style type="text/scss">
 
